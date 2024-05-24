bd1e70e827033152a63c0c0c6594029880f7f27e
Juan Carlos Hernandez <juancalihernandez@gmail.com>
2024-05-23 18:04:29 -0600

    temporal colocando supbase keys en code

--- src/helpers/clientes.js
+++ src/helpers/clientes.js
@@ -1,7 +1,6 @@
 import {createClient} from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-console.log(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = 'https://sgcojjqacyzgaolozmkp.supabase.co' //import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnY29qanFhY3l6Z2FvbG96bWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyNjYxNzUsImV4cCI6MjAzMDg0MjE3NX0.AxNIGugJgYi-OUwUFSn0bqkMXXIWDvr8QsDlhQEeTIg' //import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
